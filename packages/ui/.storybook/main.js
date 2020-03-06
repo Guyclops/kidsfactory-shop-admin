40f9e76212d3f0e096984ed60a3161043e7c9ece
juntae <wnsxo0816@gmail.com>
2020-03-06 13:58:47 +0900

    feat(ui): add storybook package

---
+++ packages/ui/.storybook/main.js
@@ -0,0 +1,25 @@
+module.exports = {
+  stories: ["../**/*.stories.(js|mdx|tsx)"],
+  addons: [
+    "@storybook/addon-actions",
+    "@storybook/addon-knobs",
+    "@storybook/addon-docs",
+    "@storybook/preset-typescript",
+  ],
+  webpackFinal: async config => {
+    config.module.rules.push({
+      test: /\.(ts|tsx)$/,
+      use: [
+        {
+          loader: require.resolve("babel-loader"),
+          options: {
+            presets: [["react-app", { flow: false, typescript: true }]],
+          },
+        },
+        require.resolve("react-docgen-typescript-loader"),
+      ],
+    });
+    config.resolve.extensions.push(".ts", ".tsx");
+    return config;
+  },
+};
